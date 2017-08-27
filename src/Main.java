a9c8a41e63a2fc95c487b9602f2b45d7d26372cf
timalperamune <timalperamune@10.129.21.133>
2017-08-26 21:01:10 -0400

    Latest Update- Timal

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,28 @@
+import java.util.List;
+
+import twitter4j.Status;
+import twitter4j.TwitterException;
+import twitter4j.TwitterFactory;
+import twitter4j.conf.ConfigurationBuilder;
 
 public class Main {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public static void main (String[]args) throws TwitterException
+	{
+		ConfigurationBuilder configurationBuilder = new ConfigurationBuilder();
+		configurationBuilder.setDebugEnabled(true).setOAuthConsumerKey("Fh7piQWQDODoDZBus4InQt18u")
+			.setOAuthConsumerSecret("actLZGHjQInGNN8WcZuUNqTMHPtOXnuTAUDqH8b47QubctrMHe")
+			.setOAuthAccessToken("346826171-DUGmKTgT7uwK4idG1kxepWVnDLIC6LEEebmyVSOx")
+			.setOAuthAccessTokenSecret("FjcdUkvbRtWecTNAL8H1h6pI0XwqJZqvAR5ghYGzt3RsQ");
+		
+		TwitterFactory tf = new TwitterFactory(configurationBuilder.build());
+		twitter4j.Twitter twitter = tf.getInstance();
+		
+		List<Status> status = twitter.getUserTimeline();
+		
+		for(Status s: status)
+		{
+			System.out.println(s.getUser().getName()+"      "+ s.getText());
+		}
 	}
-
 }
