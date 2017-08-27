a9d53c0cbab6e24e5ee1d88f184e2cd7a455a8da
timalperamune <timalperamune@10.129.21.133>
2017-08-26 22:25:56 -0400

    -Timal

--- src/FirstPage.java
+++ src/FirstPage.java
@@ -1,4 +1,39 @@
+import java.awt.Dimension;
+import java.io.IOException;
 
-public class FirstPage {
+import javax.imageio.ImageIO;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
 
+public class FirstPage extends JLabel{
+
+	public static ImageIcon IDPage;
+	JTextField textArea;
+	static {
+		ClassLoader loader = FirstPage.class.getClassLoader();
+
+		try{
+			IDPage = new ImageIcon(ImageIO.read(loader.getResource("PAGE1BACKGROUNDCOVER.PNg")));
+			
+		}catch(IOException e){
+			System.out.println("Could not find file");
+		}
+		
+		
+	}
+	
+	public FirstPage(){
+		
+		super.setIcon(IDPage);
+		this.setPreferredSize(new Dimension(375,641));
+//		this.textArea =  new JTextField(30);
+//		this.add(this.textArea);
+		JButton button = new JButton("ANALYZE");
+		this.revalidate();
+	}
+	
+	
+	
 }
--- src/Main.java
+++ src/Main.java
@@ -1,28 +1,37 @@
-import java.util.List;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
 
-import twitter4j.Status;
 import twitter4j.TwitterException;
-import twitter4j.TwitterFactory;
-import twitter4j.conf.ConfigurationBuilder;
 
 public class Main {
 
+	
 	public static void main (String[]args) throws TwitterException
 	{
-		ConfigurationBuilder configurationBuilder = new ConfigurationBuilder();
-		configurationBuilder.setDebugEnabled(true).setOAuthConsumerKey("Fh7piQWQDODoDZBus4InQt18u")
-			.setOAuthConsumerSecret("actLZGHjQInGNN8WcZuUNqTMHPtOXnuTAUDqH8b47QubctrMHe")
-			.setOAuthAccessToken("346826171-DUGmKTgT7uwK4idG1kxepWVnDLIC6LEEebmyVSOx")
-			.setOAuthAccessTokenSecret("FjcdUkvbRtWecTNAL8H1h6pI0XwqJZqvAR5ghYGzt3RsQ");
 		
-		TwitterFactory tf = new TwitterFactory(configurationBuilder.build());
-		twitter4j.Twitter twitter = tf.getInstance();
+		FirstPage page = new FirstPage();
+		JFrame frame = new JFrame();
+		JPanel panel = new JPanel();
+		panel.add(page);
+		frame.add(panel);
+		frame.pack();
+		frame.setResizable(false);
+		frame.setVisible(true);
 		
-		List<Status> status = twitter.getUserTimeline();
-		
-		for(Status s: status)
-		{
-			System.out.println(s.getUser().getName()+"      "+ s.getText());
-		}
+//		ConfigurationBuilder configurationBuilder = new ConfigurationBuilder();
+//		configurationBuilder.setDebugEnabled(true).setOAuthConsumerKey("Fh7piQWQDODoDZBus4InQt18u")
+//			.setOAuthConsumerSecret("actLZGHjQInGNN8WcZuUNqTMHPtOXnuTAUDqH8b47QubctrMHe")
+//			.setOAuthAccessToken("346826171-DUGmKTgT7uwK4idG1kxepWVnDLIC6LEEebmyVSOx")
+//			.setOAuthAccessTokenSecret("FjcdUkvbRtWecTNAL8H1h6pI0XwqJZqvAR5ghYGzt3RsQ");
+//		
+//		TwitterFactory tf = new TwitterFactory(configurationBuilder.build());
+//		twitter4j.Twitter twitter = tf.getInstance();
+//		
+//		List<Status> status = twitter.getUserTimeline();
+//		
+//		for(Status s: status)
+//		{
+//			System.out.println(s.getUser().getName()+"      "+ s.getText());
+//		}
 	}
 }
